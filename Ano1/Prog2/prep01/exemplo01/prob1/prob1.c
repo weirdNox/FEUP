b9eec82f98985ed109f9e0b92a07d956c0b7c974
Gonçalo Santos <goncalossantos98@gmail.com>
2017-03-21 21:55:22 +0000

    [Prog2] Resolver exemplo01 de preparação para o miniteste01

--- Ano1/Prog2/prep01/exemplo01/prob1/prob1.c
+++ Ano1/Prog2/prep01/exemplo01/prob1/prob1.c
@@ -11,20 +11,95 @@
 /*** problema 1.1 ***/
 float comprimento_medio(lista *lst)
 {
-	/* complexidade do algoritmo: ______________ */
-	return 0;
+	/* complexidade do algoritmo: O(n) */
+    if(!lst || lista_tamanho(lst) <= 0)
+    {
+        return 0;
+    }
+
+    float result = 0;
+    elemento *next = lista_inicio(lst);
+    while(next)
+    {
+        result += strlen(next->str);
+        next = next->proximo;
+    }
+    result /= (float)lista_tamanho(lst);
+
+    return result;
 }
 
 /*** problema 1.2 ***/
 lista* filtra_nomes(lista *lst, char *existe)
 {
-	return NULL;
+    if(!lst || !existe)
+    {
+        return 0;
+    }
+
+    lista *nova = lista_nova();
+    if(!nova)
+    {
+        return 0;
+    }
+
+    elemento *next = lista_inicio(lst);
+    while(next)
+    {
+        if(strstr(next->str, existe))
+        {
+            if(!lista_insere(nova, next->str, 0) ||
+               !(next = lista_remove(lst, next)))
+            {
+                lista_apaga(nova);
+                return 0;
+            }
+        }
+        else
+        {
+            next = next->proximo;
+        }
+    }
+
+    return nova;
 }
 
 /*** problema 1.3 ***/
 const char* mais_palavras(vetor *vec)
 {
-	return NULL;
+    int tamanho;
+    if(!vec || !(tamanho = vetor_tamanho(vec)))
+    {
+        return 0;
+    }
+
+    int max = 0;
+    int maxIdx = 0;
+    for(int i = 0; i < tamanho; ++i)
+    {
+        const char *c = vetor_elemento(vec, i);
+        if(!c)
+        {
+            return 0;
+        }
+
+        int wordCount = 1;
+        while(*c)
+        {
+            if(*(c++) == ' ')
+            {
+                ++wordCount;
+            }
+        }
+
+        if(wordCount > max)
+        {
+            max = wordCount;
+            maxIdx = i;
+        }
+    }
+
+    return vetor_elemento(vec, maxIdx);
 }
 
 /****************************************************/
@@ -35,19 +110,19 @@ vetor* lerParaVetor(FILE* ficheiro)
 {
 	char buffer[256], *nlptr;
 	vetor* v;
-	
+
 	if (ficheiro == NULL)
 		return NULL;
 
 	v = vetor_novo();
 
-	while(!feof(ficheiro)) 
-	{	
+	while(!feof(ficheiro))
+	{
 		fgets(buffer, 255, ficheiro);
 		nlptr = strchr(buffer, '\n');
-		if (nlptr) 
+		if (nlptr)
 			*nlptr = '\0';
-		
+
 		vetor_insere(v,buffer,v->tamanho);
 	}
 
@@ -58,19 +133,19 @@ lista* lerParaLista(FILE* ficheiro)
 {
 	char buffer[256], *nlptr;
 	lista* l;
-	
+
 	if (ficheiro == NULL)
 		return NULL;
 
 	l = lista_nova();
-	
-	while(!feof(ficheiro)) 
-	{	
+
+	while(!feof(ficheiro))
+	{
 		fgets(buffer, 255, ficheiro);
 		nlptr = strchr(buffer, '\n');
-		if (nlptr) 
+		if (nlptr)
 			*nlptr = '\0';
-		
+
 		lista_insere(l,buffer,NULL);
 	}
 
@@ -86,7 +161,7 @@ int main()
     const char *nome;
     int i;
     double comp;
-    
+
 	/* testes */
 	f = fopen("freguesias.txt","r");
 	if(f == NULL)
@@ -95,7 +170,7 @@ int main()
 		return 1;
 	}
 	l = lerParaLista(f);
-		
+
 	/* testa comprimento_medio */
 	comp = comprimento_medio(l);
 	if(comp)
@@ -105,7 +180,7 @@ int main()
 
 	/* testa filtra_nomes */
     resultado = filtra_nomes(l, "Mamede");
-    
+
 	if(resultado)
 	{
             printf("\nNomes filtrados (total de %d):\n", lista_tamanho(resultado));
@@ -120,18 +195,18 @@ int main()
 
 	/* testa mais_palavras */
 	rewind(f);
-	v = lerParaVetor(f);    
+	v = lerParaVetor(f);
 	nome = mais_palavras(v);
 	if(nome)
 	  	printf("\nNome com mais palavras: %s\n\n", nome);
 	else
 	   	printf("\nmais_palavras retornou NULL\n\n");
-	
+
 	vetor_apaga(v);
 	lista_apaga(l);
 	lista_apaga(resultado);
-	
+
 	fclose(f);
-		
+
 	return 0;
 }
--- Ano1/Prog2/prep01/exemplo01/prob2/prob2.c
+++ Ano1/Prog2/prep01/exemplo01/prob2/prob2.c
@@ -16,15 +16,66 @@ municipio;
 /*** problema 2.1 ***/
 municipio *lerFicheiro(FILE *ficheiro, int *num_municipios)
 {
+    if(!ficheiro)
+    {
+        return 0;
+    }
 
-	return NULL;
+    municipio *result = 0;
+
+    char buffer[1<<13];
+    while(fgets(buffer, sizeof(buffer)/sizeof(buffer[0]), ficheiro))
+    {
+        ++(*num_municipios);
+        municipio *test = realloc(result, sizeof(*result)*(*num_municipios));
+        if(!test)
+        {
+            free(result);
+            return 0;
+        }
+        result = test;
+
+        municipio *atual = result + (*num_municipios)-1;
+        char *token = strtok(buffer, "\t");
+        strncpy(atual->nome, token, 100);
+        token = strtok(0, "\t");
+        atual->pop2001 = atoi(token);
+        token = strtok(0, "\t");
+        atual->pop2012 = atoi(token);
+    }
+
+	return result;
 }
 
 /*** problema 2.2 ***/
 void gerarRelatorio(FILE *ficheiro, municipio *resultados, int num_municipios)
 {
+    if(!ficheiro)
+    {
+        return;
+    }
 
-
+    for(int i = 0; i < 10; ++i)
+    {
+        int maxIdx = i;
+        int maxPerdida = resultados[i].pop2001 - resultados[i].pop2012;
+        for(int j = i+1; j < num_municipios; ++j)
+        {
+            int testePerdida = resultados[j].pop2001 - resultados[j].pop2012;
+            if(testePerdida > maxPerdida)
+            {
+                maxIdx = j;
+                maxPerdida = testePerdida;
+            }
+        }
+        if(maxIdx != i)
+        {
+            municipio temp = resultados[i];
+            resultados[i] = resultados[maxIdx];
+            resultados[maxIdx] = temp;
+        }
+        fprintf(ficheiro, "%s, %d\n", resultados[i].nome, maxPerdida);
+    }
 }
 
 /****************************************************/
