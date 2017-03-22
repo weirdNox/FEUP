a15d2421e27d49c9cfec40417e382004fc45b57d
Gonçalo Santos <goncalossantos98@gmail.com>
2017-03-22 12:28:16 +0000

    [Prog2] Trabalho01 - Acabar implementação de vetor

--- Ano1/Prog2/trabalho01/vetor.c
+++ Ano1/Prog2/trabalho01/vetor.c
@@ -192,40 +192,12 @@ int vetor_ordena_ins(vetor* vec)
 
 /* funções a implementar */
 
-int swap(vetor *vec, int indexA, int indexB)
+// NOTE(Gonçalo): Esta função _NÃO_ verifica se o idxA e o idxB estão dentro dos limites
+void vetor__unsafeSwap(vetor *vec, int idxA, int idxB)
 {
-    if(!vec)
-    {
-        return -1;
-    }
-
-    if(indexA == indexB)
-    {
-        return 0;
-    }
-
-    const char *strA = vetor_elemento(vec, indexA);
-    const char *strB = vetor_elemento(vec, indexB);
-    if(!strA || !strB)
-    {
-        return -1;
-    }
-
-    char *tempA = malloc((strlen(strA)+1)*sizeof(*tempA));
-    if(!tempA)
-    {
-        return -1;
-    }
-
-    strcpy(tempA, strA);
-    if(vetor_atribui(vec, indexA, strB) == -1 || vetor_atribui(vec, indexB, tempA) == -1)
-    {
-        return -1;
-    }
-
-    free(tempA);
-
-    return 0;
+    v_elemento tempA = vec->elementos[idxA];
+    vec->elementos[idxA] = vec->elementos[idxB];
+    vec->elementos[idxB] = tempA;
 }
 
 int vetor_ordena_sel(vetor* vec)
@@ -235,121 +207,65 @@ int vetor_ordena_sel(vetor* vec)
         return -1;
     }
 
-    for(int index = 0; index < vec->tamanho-1; ++index)
+    int size = vec->tamanho;
+    for(int i = 0; i < size; ++i)
     {
-        int minIndex = index;
-        const char *minStr = vetor_elemento(vec, index);
-        if(!minStr)
+        int iMin = i;
+        for(int j = i+1; j < size; ++j)
         {
-            return -1;
-        }
-
-        for(int testIndex = index+1; testIndex < vec->tamanho; ++testIndex)
-        {
-            const char *testStr = vetor_elemento(vec, testIndex);
-            if(!testStr)
+            if(strcmp(vec->elementos[j].str, vec->elementos[iMin].str) < 0)
             {
-                return -1;
-            }
-
-            if(strcmp(testStr, minStr) < 0)
-            {
-                minIndex = testIndex;
-                minStr = testStr;
+                iMin = j;
             }
         }
-
-        if(swap(vec, minIndex, index) == -1)
+        if(iMin != i)
         {
-            return -1;
+            vetor__unsafeSwap(vec, iMin, i);
         }
     }
 
-	return 0;
+    return 0;
 }
 
-int quicksortSlowPartition(vetor *vec, int low, int high)
+void vetor__qsortPartition(vetor *vec, int low, int high)
 {
     int size = high - low + 1;
     if(size < 2)
     {
-        // NOTE(Gonçalo): Está feito
-        return 0;
+        return;
     }
 
-#if 0
-    // NOTE(Gonçalo): Semelhante ao algoritmo de Lomuto
-    swap(vec, (rand() % size) + low, high); // NOTE(Gonçalo): Pivot está no índice high
-    const char *pivot = vetor_elemento(vec, high);
-    if(!pivot)
-    {
-        return -1;
-    }
-
-    int boundary = low, unknownStart = low;
-    while(unknownStart < high)
-    {
-        const char *test = vetor_elemento(vec, unknownStart);
-        if(!test)
-        {
-            return -1;
-        }
-
-        if(strcmp(test, pivot) <= 0)
-        {
-            if(swap(vec, boundary++, unknownStart++) == -1)
-            {
-                return -1;
-            }
-        }
-        else
-        {
-            ++unknownStart;
-        }
-    }
-
-    swap(vec, high, boundary);
-
-    quicksortSlowPartition(vec, low, boundary-1);
-    quicksortSlowPartition(vec, boundary+1, high);
-#else
-    // NOTE(Gonçalo): Semelhante ao algoritmo de Hoare
-    int pivotPos = (rand() % size) + low;
-    swap(vec, pivotPos, high);
-    const char *pivot = vetor_elemento(vec, high);
-    if(!pivot)
-    {
-        return -1;
-    }
+    int pivotIdx = (rand() % size) + low;
+    vetor__unsafeSwap(vec, pivotIdx, high);
+    char *pivot = vec->elementos[high].str;
 
     int left = low, right = high-1;
     for(;;)
     {
-        while(left < high && strcmp(vetor_elemento(vec, left), pivot) <= 0)
+        while(left < high && strcmp(vec->elementos[left].str, pivot) <= 0)
         {
             ++left;
         }
-        while(right >= 0 && strcmp(vetor_elemento(vec, right), pivot) > 0)
+        while(right > low && strcmp(vec->elementos[right].str, pivot) > 0)
         {
             --right;
         }
 
         if(left < right)
         {
-            swap(vec, left, right);
+            vetor__unsafeSwap(vec, left, right);
         }
         else
         {
             break;
         }
     }
-    swap(vec, left, high);
+    vetor__unsafeSwap(vec, left, high); // NOTE(Gonçalo): Restaurar pivot em left
 
-    quicksortSlowPartition(vec, low, left-1);
-    quicksortSlowPartition(vec, left+1, high);
-#endif
+    vetor__qsortPartition(vec, low, left-1);
+    vetor__qsortPartition(vec, left+1, high);
 
-    return 0;
+    return;
 }
 
 int vetor_ordena_qsort(vetor* vec)
@@ -358,85 +274,97 @@ int vetor_ordena_qsort(vetor* vec)
     {
         return -1;
     }
-
-	return quicksortSlowPartition(vec, 0, vec->tamanho-1);
+	vetor__qsortPartition(vec, 0, vec->tamanho-1);
+    return 0;
 }
 
-int getPivot(vetor *vec, int low, int high)
+int vetor__qsortMedian(vetor *vec, int low, int high)
 {
-    const char *lowStr = vetor_elemento(vec, low);
-    const char *middleStr = vetor_elemento(vec, (low+high)/2);
-    const char *highStr = vetor_elemento(vec, high);
+    int middle = (low+high)/2;
 
-    if(!lowStr || !middleStr || !highStr)
-    {
-        return -1;
-    }
+    int lowMid = strcmp(vec->elementos[low].str, vec->elementos[middle].str);
+    int midHigh = strcmp(vec->elementos[middle].str, vec->elementos[high].str);
+    int lowHigh = strcmp(vec->elementos[low].str, vec->elementos[high].str);
 
-    int lowMiddle = strcmp(lowStr, middleStr);
-    int middleHigh = strcmp(middleStr, highStr);
-    int lowHigh = strcmp(lowStr, highStr);
-
-    if((lowMiddle > 0 && lowHigh < 0) ||
-       (lowMiddle < 0 && lowHigh > 0))
+    if((lowMid > 0 && lowHigh < 0) ||
+       (lowMid < 0 && lowHigh > 0))
     {
         return low;
     }
-    if((lowMiddle < 0 && middleHigh < 0) ||
-       (lowMiddle > 0 && middleHigh > 0))
+    if((lowMid < 0 && midHigh < 0) ||
+       (lowMid > 0 && midHigh > 0))
     {
-        return (low+high)/2;
+        return middle;
     }
-    else
-    {
-        return high;
-    }
+
+    return high;
 }
 
-int quicksortFastPartition(vetor *vec, int low, int high)
+/* NOTE(Gonçalo):
+    Otimizações em vigor:
+     - Quando tamanho menor que um threshold, usar Insertion Sort
+     - Escolher pivot como mediana dos extremos e o valor médio
+     - Usar um algoritmo de partição em 3 partes (Dutch National Flag problem). Temos
+       os elementos menor que o pivot, iguais e maiores
+*/
+void vetor__qsortBetterPartition(vetor *vec, int low, int high)
 {
     int size = high - low + 1;
-    if(size < 2)
+    if(size < 150)
     {
-        // NOTE(Gonçalo): Está feito
-        return 0;
-    }
-
-    int pivotPos = getPivot(vec, low, high);
-    swap(vec, pivotPos, high);
-    const char *pivot = vetor_elemento(vec, high);
-    if(!pivot)
-    {
-        return -1;
-    }
-
-    int left = low, right = high-1;
-    for(;;)
-    {
-        while(left < high && strcmp(vetor_elemento(vec, left), pivot) <= 0)
+        // NOTE(Gonçalo): Sort por inserção
+        for(int i = low+1; i <= high; ++i)
         {
-            ++left;
+            char *temp = vec->elementos[i].str;
+            int j;
+            for(j = i; j > low && strcmp(temp, vec->elementos[j-1].str) < 0; --j)
+            {
+                vec->elementos[j] = vec->elementos[j-1];
+            }
+            vec->elementos[j].str = temp;
         }
-        while(right >= 0 && strcmp(vetor_elemento(vec, right), pivot) > 0)
+        return;
+    }
+
+    // NOTE(Gonçalo): Dutch National Flag Problem
+    // Three-way partition
+    int pivotIdx = vetor__qsortMedian(vec, low, high);
+    char *pivot = vec->elementos[pivotIdx].str;
+
+    int left = low, iter = low;
+    int right = low + size - 1;
+    while(iter <= right)
+    {
+        int comp = strcmp(vec->elementos[iter].str, pivot);
+        if(comp < 0)
         {
+            // NOTE(Gonçalo): Menor que pivot
+            vetor__unsafeSwap(vec, left, iter);
+            ++left; ++iter;
+        }
+        else if(comp > 0)
+        {
+            // NOTE(Gonçalo): Maior que pivot
+            vetor__unsafeSwap(vec, iter, right);
             --right;
-        }
-
-        if(left < right)
-        {
-            swap(vec, left, right);
         }
         else
         {
-            break;
+            // NOTE(Gonçalo): Igual
+            ++iter;
         }
     }
-    swap(vec, left, high);
 
-    quicksortSlowPartition(vec, low, left-1);
-    quicksortSlowPartition(vec, left+1, high);
+    if(left > low)
+    {
+        vetor__qsortBetterPartition(vec, low, left-1);
+    }
+    if(right < high)
+    {
+        vetor__qsortBetterPartition(vec, right+1, high);
+    }
 
-    return 0;
+    return;
 }
 
 int vetor_ordena(vetor* vec)
@@ -446,7 +374,9 @@ int vetor_ordena(vetor* vec)
         return -1;
     }
 
-	return quicksortFastPartition(vec, 0, vec->tamanho-1);
+    vetor__qsortBetterPartition(vec, 0, vec->tamanho-1);
+
+	return 0;
 }
 
 vetor* le_ficheiro(char* nome)
@@ -454,28 +384,27 @@ vetor* le_ficheiro(char* nome)
     FILE *file = fopen(nome, "r");
     if(!file)
     {
-        return NULL;
+        return 0;
     }
 
     vetor *result = vetor_novo();
     if(!result)
     {
         fclose(file);
-        return NULL;
+        return 0;
     }
 
-    char buffer[1<<10];
+    char buffer[1<<11];
     while(fgets(buffer, sizeof(buffer)/sizeof(buffer[0]), file))
     {
-        // NOTE(Gonçalo): Remover \n
-        buffer[strlen(buffer)-1] = 0;
+        buffer[strlen(buffer)-1] = 0; // NOTE(Gonçalo): Remover \n
         if(vetor_insere(result, buffer, -1) == -1)
         {
             vetor_apaga(result);
             fclose(file);
-            return NULL;
+            return 0;
         }
     }
 
-    return result;
+	return result;
 }
