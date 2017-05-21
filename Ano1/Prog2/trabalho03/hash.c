c9f16697a331b6362c7f0fbc663836221769321a
Gonçalo Santos <goncalossantos98@gmail.com>
2017-05-21 23:08:59 +0100

    [Prog2] Trabalho 3 - Últimas otimizações

--- Ano1/Prog2/trabalho03/hash.c
+++ Ano1/Prog2/trabalho03/hash.c
@@ -3,24 +3,39 @@
 #include <string.h>
 #include <stdint.h>
 
-int initHashTable(HashTable *table, long int maxFilled)
+int initHashTable(HashTable *table, uint32_t maxFilled)
 {
-    if(!table || maxFilled <= 0 || table->buckets)
+    if(!table || maxFilled == 0 || maxFilled > INT32_MAX || table->buckets)
     {
         return 0;
     }
 
+    // NOTE(nox): We want to keep a maximum load factor of 80%
+    uint32_t numBuckets = maxFilled / 0.8f;
+    if(numBuckets > INT32_MAX)
+    {
+        numBuckets = 0x80000000;
+    }
+    else
+    {
+        // NOTE(nox): Round to next power of two
+        numBuckets -= 1;
+        numBuckets |= numBuckets >> 1;
+        numBuckets |= numBuckets >> 2;
+        numBuckets |= numBuckets >> 4;
+        numBuckets |= numBuckets >> 8;
+        numBuckets |= numBuckets >> 16;
+        numBuckets++;
+    }
+
     table->usedEntries = 0;
     table->availableSpace = maxFilled;
-    table->entriesMemory = calloc(table->availableSpace, sizeof(*table->entriesMemory));
-
-    // NOTE(nox): We want to keep a maximum load factor of 60%
-    table->numBuckets = maxFilled / 0.6f;
+    table->numBuckets = numBuckets;
+    table->moduloMask = numBuckets-1;
     table->buckets = calloc(table->numBuckets, sizeof(*table->buckets));
-    if(!table->entriesMemory || !table->buckets)
+    if(!table->buckets)
     {
         free(table->buckets);
-        free(table->entriesMemory);
         *table = (HashTable){};
         return 0;
     }
@@ -35,9 +50,7 @@ void freeHashTable(HashTable *table)
         return;
     }
 
-    free(table->entriesMemory);
     free(table->buckets);
-
     *table = (HashTable){};
 }
 
@@ -56,19 +69,23 @@ HashElement *hashTableFetch(HashTable *table, char *name)
     {
         hash = hash * 33 + c;
     }
-    hash %= table->numBuckets;
+    hash &= table->moduloMask;
 
     HashElement *result;
     for(;;)
     {
-        result = table->buckets[hash];
+        result = table->buckets + hash;
 
-        if(!result)
+        if(result->companyName[0] == '\0')
         {
             if(table->usedEntries < table->availableSpace)
             {
-                result = table->buckets[hash] = table->entriesMemory + table->usedEntries++;
+                ++table->usedEntries;
                 strcpy(result->companyName, name);
+            }
+            else
+            {
+                result = 0;
             }
 
             break;
@@ -78,7 +95,7 @@ HashElement *hashTableFetch(HashTable *table, char *name)
             break;
         }
 
-        hash = (hash + 1) % table->numBuckets;
+        hash = (hash + 1) & table->moduloMask;
     }
 
     return result;
--- Ano1/Prog2/trabalho03/hash.h
+++ Ano1/Prog2/trabalho03/hash.h
@@ -1,28 +1,35 @@
 #if !defined(HASH_H)
 
+#include <stdint.h>
+
 typedef struct HashElement
 {
     int numberOfTransactions;
-    char companyName[20];
+    char companyName[16];
 } HashElement;
 
 // Open addressed Hash Table
+// It will  have some  available space  and a bigger  number of  buckets, however,  if the
+// number of  used entries gets to  available space, no more  entries can be added  to the
+// hash table, even with empty buckets. This serves to enforce the ~70-80% load factor.
+// Also, even when full, there will be unused memory, but it was designed with performance
+// in mind.
 typedef struct
 {
-    HashElement **buckets;
-    HashElement *entriesMemory;
-    long int numBuckets;
-    long int availableSpace;
-    long int usedEntries;
+    HashElement *buckets;
+    uint32_t usedEntries;
+    uint32_t availableSpace;
+    uint32_t numBuckets;
+    uint32_t moduloMask;
 } HashTable;
 
 // Initializes Hash Table passed by pointer
 // If table is already initialized, won't do anything
 // Return 0 on error, 1 on success
-int initHashTable(HashTable *table, long int size);
+int initHashTable(HashTable *table, uint32_t size);
 
 // Free table buckets
-// Will set table members to null on success
+// Will set struct members to null on success
 void freeHashTable(HashTable *table);
 
 // Fetch table element by name, or create it if it doesn't exist
