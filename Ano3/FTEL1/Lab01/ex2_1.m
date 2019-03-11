9439badb31327c21d0ef33622c2a31e858279e89
Gonçalo Santos <goncalossantos98@gmail.com>
2019-03-11 16:13:12 +0000

    [FTEL1] Finalização Lab 1

--- Ano3/FTEL1/Lab01/ex2_1.m
+++ Ano3/FTEL1/Lab01/ex2_1.m
@@ -1,13 +1,15 @@
-Max =  1.3;
+Max =  1.5;
 Min = -Max;
 X = [1.2 -0.4 0.1];
-Zc = compand(X, 10, Max, 'mu/compressor');
+
+Miu = 10;
+Zc = compand(X, Miu, Max, 'mu/compressor');
 
 N = 4;
 [Boundary, Codebook, Delta] = quantizationInfo(N, Min, Max);
 [~, Zq] = quantiz(Zc, Boundary, Codebook);
 
-Z = compand(Zq, 10, Max, 'mu/expander');
+Z = compand(Zq, Miu, Max, 'mu/expander');
 
 %% figure(1); clf; hold on;
 %% plot(1:length(X), X);
--- Ano3/FTEL1/Lab01/ex2_2.m
+++ Ano3/FTEL1/Lab01/ex2_2.m
@@ -1,6 +1,3 @@
-L = 32;
-N = log2(L);
-A = 87.6;
 Max = 1;
 Min = -Max;
 
@@ -10,12 +7,44 @@ TSim = 1;
 T = [0:Ts:TSim];
 X = (3*cos(40*pi*T) + 2*sin(240*pi*T))*(1/4.902);
 
-[Boundary, Codebook, Delta] = quantizationInfo(N, Min, Max);
+N = [3, 5, 7];
 
-Xc = compand(X, A, Max, 'A/compressor');
-[~, Xq] = quantiz(Xc, Boundary, Codebook);
-Z = compand(Xq, A, Max, 'A/expander');
+quantAnalysis(X,    N, Min, Max);
+quantAnalysis(X/10, N, Min, Max);
 
-figure(1); clf; hold on;
-plot(T, X);
-plot(T, Z, 'o');
+fprintf('Q1: SNR não varia tanto com a quantização não uniforme (V/F) -> %s\n', 'V');
+
+function [SigPower, NoisePower, SnrDb, NoisePowerA, SnrDbA] = compQuantizExp(X, N, Min, Max)
+  SigPower = mean(X.^2);
+  [Boundary, Codebook, Delta] = quantizationInfo(N, Min, Max);
+
+  %% ------------------------------------------------------------------------------------------
+  %% Non-uniform quantization: Lei A
+  [~, XQuant] = quantiz(X, Boundary, Codebook);
+
+  QuantErr = X - XQuant;
+  NoisePower = mean(QuantErr.^2);
+
+  SnrDb = 10*log10(SigPower/NoisePower);
+
+  %% ------------------------------------------------------------------------------------------
+  %% Non-uniform quantization: Lei A
+  A = 87.6;
+  XComp = compand(X, A, Max, 'A/compressor');
+  [~, XCompQuant] = quantiz(XComp, Boundary, Codebook);
+  XExpanded = compand(XCompQuant, A, Max, 'A/expander');
+
+  QuantErrA = X - XExpanded;
+  NoisePowerA = mean(QuantErrA.^2);
+
+  SnrDbA = 10*log10(SigPower/NoisePowerA);
+end
+
+function quantAnalysis(X, N, Min, Max)
+  fprintf('%-1s  %-4s  %-12s  %-12s  %-16s  %-16s  %-16s\n', 'N', 'L', 'Potência (W)', 'Ruído (W)', 'SNR Unif (dB)', 'Ruído Lei A (W)', 'SNR Lei A (dB)');
+  for Idx = 1:length(N)
+    [SigPower, NoisePower, SnrDb, NoisePowerA, SnrDbA]= compQuantizExp(X, N(Idx), Min, Max);
+    fprintf('%-1i  %-4i  %-12g  %-12g  %-16g  %-16g  %-16g\n', N(Idx), 2^N(Idx), SigPower, NoisePower, SnrDb, NoisePowerA, SnrDbA);
+  end
+  fprintf('\n')
+end
