dc8771e1f91f3ba4ad115d17e654a31cc0d91da1
Gonçalo Santos <goncalossantos98@gmail.com>
2019-06-01 12:11:58 +0100

    [ELEC3] Script sym2tf

---
+++ Ano3/ELEC3/scripts/sym2tf.m
@@ -0,0 +1,20 @@
+
+%% Example usage:
+%%   bode(sym2tf(subs(Wn^2 / (s^2 + s*2*Zeta*Wn + Wn^2), {Wn, Zeta}, {2*pi*10e3, 1/sqrt(2)})))
+
+function [ tfobj ] = sym2tf( symobj, Ts)
+  if isnumeric(symobj)
+    tfobj=symobj;
+    return;
+  end
+
+  [n,d]=numden(symobj);
+  num=sym2poly(n);
+  den=sym2poly(d);
+
+  if nargin==1
+    tfobj=tf(num,den);
+  else
+    tfobj=tf(num,den,Ts);
+  end
+end
