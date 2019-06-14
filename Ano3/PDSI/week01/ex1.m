8cfc4e582d43833fa868788a7d0e96bed6cbfc54
Gonçalo Santos <goncalossantos98@gmail.com>
2019-06-14 16:57:05 +0100

    [PDSI] Complemento na semana 1

--- Ano3/PDSI/week01/ex1.m
+++ Ano3/PDSI/week01/ex1.m
@@ -1,20 +1,20 @@
 Num = 0;
 while Num < 30
-    Num = input('Insert a number ');
+    Num = input('Insert a number (>= 30) ');
 end
 NumSequence = [0:Num-1];
 
 Alpha = 0.95*exp(j*pi/3);
 H = Alpha.^NumSequence;
 
-figure(1);
+figure(1); clf;
 plot(NumSequence, abs(H));
 title('Complex exponential');
 xlabel('NumSequence \rightarrow');
 ylabel('Magnitude \rightarrow');
 legend('Envelope');
 
-figure(2);
+figure(2); clf;
 subplot(3, 1, 1);
 stem(NumSequence, real(H));
 
@@ -25,3 +25,18 @@ stem(NumSequence, X);
 subplot(3, 1, 3);
 Y = conv(X, real(H));
 stem(NumSequence, Y(1:Num), 'pentagram');
+
+%% NOTE(nox): Extra questions
+%% i) sum(H .* conj(H)) → Sum of the element-wise multiplication between H and its conjugate
+%%    H * H' → Calculates the conjugate transposition of H and multiplies it with H
+%%
+%%    Both are the dot product of H with itself (complex dot produt).
+
+%% ii) For real vectors, there is no difference between ' and .'; however, for complex vectors, it is an
+%%     alternative definition for the dot product that loses some properties.
+%%     H .* H is just the element-wise multiplication with itself.
+
+%% iii) We could have used filter with 1 as the denominator; the difference is that the length of the
+%%      returned vector would already be the size of the input vector
+
+%% iv) whos is the long form version of who
